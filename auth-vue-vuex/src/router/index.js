c029133e818145f208b4d70c623861d1cdf11b26
Adriano <adriano.faria@outlook.com.br>
2021-07-23 13:51:38 -0300

    Basic login

--- auth-vue-vuex/src/router/index.js
+++ auth-vue-vuex/src/router/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Gerentes from '../views/Gerentes.vue'
 import Home from '../views/Home.vue'
+import NovoUsuario from '../views/NovoUsuario'
+import Login from '../views/Login'
 
 Vue.use(VueRouter)
 
@@ -15,7 +17,16 @@ const routes = [
     path: '/gerentes',
     name: 'gerentes',
     component: Gerentes
-  }
+  },{
+    path: '/cadastre-se',
+    name: 'novo.usuario',
+    component: NovoUsuario
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: Login    
+  }  
 ]
 
 const router = new VueRouter({
