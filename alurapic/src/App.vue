7d65f0caea3ab9d8bfe2d804155d348823ab21da
Adriano <adriano.faria@outlook.com.br>
2021-07-23 11:51:56 -0300

    iniciando

---
+++ alurapic/src/App.vue
@@ -0,0 +1,43 @@
+<!-- alurapic/src/App.vue -->
+
+<template>
+  <div class="container">
+    <meu-menu :rotas="routes" />
+
+    <transition name="pagina">
+      <router-view></router-view>
+    </transition>
+  </div>
+</template>
+<script>
+import { routes } from "./routes";
+import Menu from "./components/shared/menu/Menu.vue";
+
+export default {
+  components: {
+    "meu-menu": Menu,
+  },
+
+  data() {
+    return {
+      routes: routes.filter((route) => route.menu),
+    };
+  },
+};
+</script>
+<style>
+.corpo {
+  font-family: Helvetica, sans-serif;
+  margin: 0 auto;
+  width: 96%;
+}
+
+.pagina-enter-active,
+.pagina-leave-active {
+  transition: opacity 0.3s;
+}
+.pagina-enter,
+.pagina-leave-active {
+  opacity: 0;
+}
+</style>
---
+++ alurapic/src/components/cadastro/Cadastro.vue
@@ -0,0 +1,139 @@
+<!-- alurapic/src/components/cadastro/Cadastro.vue -->
+
+<template>
+  <div>
+    <h1 class="centralizado">Cadastro</h1>
+    <h1 v-if="foto._id" class="centralizado">
+      Alteração da foto {{ foto.titulo }}
+    </h1>
+    <h1 v-else class="centralizado">Inclusão</h1>
+
+    <form @submit.prevent="grava()">
+      <div class="controle">
+        <label for="titulo">TÍTULO</label>
+        <input
+          name="titulo"
+          v-model="foto.titulo"
+          id="titulo"
+          autocomplete="off"
+          v-validate
+          data-vv-rules="required|min:3|max:30"
+          data-vv-as="título"
+        />
+        <span class="erro" v-show="errors.has('titulo')">{{
+          errors.first("titulo")
+        }}</span>
+      </div>
+
+      <div class="controle">
+        <label for="url">URL</label>
+        <input
+          name="url"
+          v-model="foto.url"
+          id="url"
+          autocomplete="off"
+          v-validate
+          data-vv-rules="required"
+        />
+        <span class="erro" v-show="errors.has('url')">{{
+          errors.first("url")
+        }}</span>
+        <imagem-responsiva
+          v-show="foto.url"
+          :url="foto.url"
+          :titulo="foto.titulo"
+        />
+      </div>
+
+      <div class="controle">
+        <label for="descricao">DESCRIÇÃO</label>
+        <textarea
+          v-model="foto.descricao"
+          id="descricao"
+          autocomplete="off"
+        ></textarea>
+      </div>
+
+      <div class="centralizado">
+        <meu-botao rotulo="GRAVAR" tipo="submit" />
+        <router-link :to="{ name: 'home' }"
+          ><meu-botao rotulo="VOLTAR" tipo="button"
+        /></router-link>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+import ImagemResponsiva from "../shared/imagem-responsiva/ImagemResponsiva.vue";
+import Botao from "../shared/botao/Botao.vue";
+
+// importando a classe Foto
+import Foto from "../../domain/foto/Foto.js";
+import FotoService from "../../domain/foto/FotoService";
+
+export default {
+  components: {
+    "imagem-responsiva": ImagemResponsiva,
+    "meu-botao": Botao,
+  },
+
+  data() {
+    return {
+      foto: new Foto(),
+      id: this.$route.params.id,
+    };
+  },
+
+  methods: {
+    grava() {
+      this.$validator.validateAll().then((success) => {
+        if (success) {
+          this.service.cadastra(this.foto).then(
+            () => {
+              if (this.id) this.$router.push({ name: "home" });
+              this.foto = new Foto();
+            },
+            (err) => console.log(err)
+          );
+        }
+      });
+    },
+  },
+
+  created() {
+    this.service = new FotoService(this.$resource);
+    if (this.id) {
+      this.service.busca(this.id).then((foto) => (this.foto = foto));
+    }
+  },
+};
+</script>
+<style scoped>
+.centralizado {
+  text-align: center;
+}
+.controle {
+  font-size: 1.2em;
+  margin-bottom: 20px;
+}
+.controle label {
+  display: block;
+  font-weight: bold;
+}
+
+.controle label + input,
+.controle textarea {
+  width: 100%;
+  font-size: inherit;
+  border-radius: 5px;
+}
+
+.centralizado {
+  text-align: center;
+}
+
+.erro {
+  color: red;
+}
+</style>
